1251bedda2fadcd363e161bb60d23baf34ac4839
Alexandre Wotawa <awotawa@gmail.com>
2022-09-29 16:51:17 +0200

    feat: implement style component and call in view for red box and green title

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -29,10 +29,29 @@ import {
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 2,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#FF0000', //red
+    color: '#00FF00',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
