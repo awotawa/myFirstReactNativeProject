260874ead742d5a99d3b7c5074847b0fad3b1790
Alexandre Wotawa <awotawa@gmail.com>
2022-10-03 10:51:10 +0200

    chore: import data from pokemons.json into Pokemons variable

--- AwesomeTSProject/App.tsx
+++ AwesomeTSProject/App.tsx
@@ -11,6 +11,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Pokemons = require('./pokemons.json');
+
 const App = () => {
   return (
     <View style={styles.container}>
